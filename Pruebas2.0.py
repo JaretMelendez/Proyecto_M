0888b2036a5bb9a26f3c597cae8ba1183feb4e75
JaretMelendez <jaretsin@hotmail.com>
2022-11-06 22:36:12 -0600

    Actualizacion 06-11-20

--- Pruebas2.0.py
+++ Pruebas2.0.py
@@ -5,72 +5,106 @@ import math
 import random
 import statistics
 import matplotlib.pyplot as plt
-
+import time
+from pyfirmata import Arduino, util
 from pyautocad import Autocad, APoint
-from numpy import random as rd
+from numpy import random as rd, np
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib import gridspec
 
-# --------------- Motor ------------------
+# --------------- Giro de Motor ------------------
+def girar_derecha(DIR,PUL,velocidad,arduino):
+    arduino.digital[DIR].write(0)
+    arduino.digital[PUL].write(1)
+    time.sleep(velocidad)
+    arduino.digital[PUL].write(0)
+    time.sleep(velocidad)
 
+# --------------- Motor ------------------
+def Motor(muestras,acumulador,Puerto_serial):
+    DIR = 2 
+    PUL = 3
+    velocidad = 0.5
+    pulsos = 30
+    i = 0
+    arduino = Arduino(Puerto_serial)
+    time.sleep(2)
+    for i in range(pulsos):
+        girar_derecha(DIR,PUL,velocidad,arduino)
+    acumulador = acumulador + i
+    if(acumulador == pulsos):
+        arduino.digital[PUL].write(0)
+        arduino.close()
+    return acumulador
 
 # --------------- Toma de muestras ------------------
-def Camara():
+def Camara(muestra):
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    step = 1
-    i=0
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        cv2.imwrite("img"+str(i+6)+".png",frame)
-        if i == step-1:
-            break
-        i= i+1
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        return 0
+    cv2.imwrite("Muestra"+str(muestra)+".png",frame)
+
 
 # --------------- Procesamiento de la imagen --------
 def Mascara(imagen):
-    imageng = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
-    [l,a] = numpy.shape(imageng)
-    final = 0
-    newi2 = numpy.zeros((l,a),dtype=numpy.uint8)
-    for i in range(l):
-        for j in range(a):
-            if(imageng[i,j] >= 30):
-                final = j
-        if(numpy.mean(imageng[i,final]) != 0):
-            newi2[i,final] = 255
-        final = 0
-    return newi2
+    Imagen_Gray = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+    [l,a] = numpy.shape(Imagen_Gray)
+    Posicion_final = 0
+    longitud_pixeles = 0
+    vector_posiciones = []
+    Posiciones_columnas = []
+    Nueva_imagen = numpy.zeros((l,a),dtype=numpy.uint8)
+    for Columnas in range(l):
+        for Filas in range(a):
+            if(Imagen_Gray[Columnas,Filas] >= 30):
+                Posicion_final = Filas
+        if(numpy.mean(Imagen_Gray[Columnas,Posicion_final]) != 0):
+            Nueva_imagen[Columnas,Posicion_final] = 255
+            longitud_pixeles = longitud_pixeles + 1
+            vector_posiciones.append(Posicion_final)
+            Posiciones_columnas.append(Columnas)
+        Posicion_final = 0
+    Coordenadas(Nueva_imagen,longitud_pixeles,Posiciones_columnas,vector_posiciones)
 
 # --------------- Coordenadas ------------------
+def Coordenadas(Imagen,longitud_pixeles,Posiciones_columnas,vector_posiciones):
+    FOV = 845
+    Co = 70
+    b = 195
+    Z0 = 400
+    Z1 = 250
+
+# ---------------------------------------------------    
+    Autocad()
 
 # --------------- Conexion Autocad ------------------
-def Autocad(datos123):
+def Autocad(coordenadas):
     acad = Autocad(create_if_not_exists=True)
     print(acad.doc.Name)
     acad.prompt("Hello, Autocad fron Python\n")     
-    b = len(datos123)-1
-    for i in range(b):
-        p1 = APoint(datos123[i])
-        p2 = APoint(datos123[i+1]) 
+    tamaño = len(coordenadas)-1
+    for contador in range(tamaño):
+        p1 = APoint(coordenadas[contador])
+        p2 = APoint(coordenadas[contador+1]) 
         line = acad.model.AddLine(p1,p2)
         line.Color = 10 
+
 # --------------- Funcion Principal ------------------
 def main():
-    imagen = cv2.imread('im.png')
-    Imagen = Mascara(imagen)
-    Autocad()
+    acumulador_motor = 0
+    Muestras = 92
+    Puerto_serial = ("COM4")
+    for i in range(Muestras):
+        if(acumulador_motor != 0):
+            acumulador_motor = retorno_motor
+        retorno_motor = Motor(Muestras,acumulador_motor,Puerto_serial)
+        Camara(i)
+    for j in range(Muestras):
+        imagen = cv2.imread("Muestra"+str(j)+".png")
+        Mascara(imagen)
 
 if __name__ == "__main__":
     main()
